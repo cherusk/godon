ad3d7930cb8e3d11f69d59241b63a87bc95e9ab6
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2020-07-05 10:28:42 +0200

    breeder loop: carve out draft net stack optimizer loop
    More of POC for interplay with airflow and rest of godon components.

---
+++ breeder/loops/lnx_net_stack.py
@@ -0,0 +1,57 @@
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+DEFAULTS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'retries': 0,
+    'trigger_rule': 'all_success'
+    # 'email': ['airflow@example.com'],
+    # 'email_on_failure': False,
+    # 'email_on_retry': False,
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    }
+
+net_stack_dag = DAG(
+        'lnx_net_stack',
+        default_args=DEFAULTS,
+        description='control loop geared to optimizing \
+                    linux network stack dynamics'
+)
+
+# perform reconnaisance at target instance
+recon_step = BashOperator(
+    task_id='reconnaisance',
+    bash_command='echo noop',
+    dag=net_stack_dag,
+)
+
+# perform optimiziation run
+optimizing_step = BashOperator(
+    task_id='optimize',
+    bash_command='echo noop',
+    dag=net_stack_dag,
+)
+
+# perform config effectuation at target instance
+effectuation_step = BashOperator(
+    task_id='effectuation',
+    bash_command='echo noop',
+    dag=net_stack_dag,
+)
+
+recon_step >> optimizing_step >> effectuation_step
