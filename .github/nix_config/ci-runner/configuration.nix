73cf9615044f910f9568ad4c86d06228d4aca5e7
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2022-01-03 12:01:21 +0100

    nixos runner: apply manual test results

--- .github/nix_config/ci-runner/configuration.nix
+++ .github/nix_config/ci-runner/configuration.nix
@@ -1,4 +1,4 @@
-##
+# #
 ## Copyright (c) 2019 Matthias Tafelmeier
 ##
 ## This file is part of godon
@@ -19,30 +19,43 @@
 
 { config, pkgs, lib, ... }:
 
-{
+#let
+#      flowgrind_image = pkgs.dockerTools.buildImage {
+#            name = "flowgrind";
+#            tag = "latest";
+#      
+#            fromImage = "ubuntu";
+#            fromImageTag = "20.04";
+#      
+#            runAsRoot = ''
+#               #!${pkgs.runtimeShell}
+#               apt-get update && apt-get install -y flowgrind
+#            '';
+#      
+#            config = {
+#                  Cmd = [ "sleep infinity"  ];
+#            };
+#        };
+#in
 
+{
   imports = [ ./hardware-configuration.nix ];
 
   boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      generationsDir.copyKernels = true; # copy files to /boot
-    };
+    loader = { grub.enable = true; };
     kernelPackages = pkgs.linuxPackages_5_10;
   };
 
   services = {
-      openssh.enable = true;
-      services.github-runner = {
-        url = https://github.com/cherusk/godon;
-        tokenFile = "/srv/gh_runner.token";
-        extraLabels = [ "nixos" "osuosl" ];
-      };
+    openssh.enable = true;
+    github-runner = {
+      url = "https://github.com/cherusk/godon";
+      tokenFile = "/srv/gh_runner.token";
+      extraLabels = [ "nixos" "osuosl" ];
+    };
   };
 
-  environment.systemPackages = let
-    pythonModules = pythonPackages: with pythonPackages; [ ];
+  environment.systemPackages = let pythonModules = pythonPackages: with pythonPackages; [ pyyaml ];
   in with pkgs; [
     (python3.withPackages pythonModules)
     ansible
@@ -68,7 +81,6 @@
     parted
     pciutils
     rsync
-    rust
     tcpdump
     tcpflow
     traceroute
@@ -79,41 +91,23 @@
     vim
   ];
 
-  users.users.godon= {
+  users.users.godon = {
     isNormalUser = true;
     home = "/home/godon/";
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [ "wheel" "docker" "libvirtd" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLt7w6tl++WJT/Bn1HsOep25KpgL867SuZAnK5ifOgd6wS0PxHGiCD/4A0RSHdHZTXMv37DGlC36rhlWJUdsncI5O1F5Ay2d626fPg+REaGi8R7Hox/qInkDs9/NS2IbZvQaYIdeDK151McagWijRFCxTEMYftpslXWKiN5ckP/KjFdUkS7fKcGwqk+UFy7ehMF42i8MUOdkZMJwy8Yc2y4FTdzlrh8VELkMxCePcU9LrvwY5sSh9j65Q5IcIBlma36JMJZQLuEWi7oEGAASehUNeMXs9WFkbyCCWdR1PjHNfbrgZUpgw71SkiQrM8+9/jcfyl5kfkn+GUECQcWBDQnbi809re1ZVmOSUrDBAoEAevFO8qHLgz0d9H0Zs/EOghyN+a4eBLB3et46F733GYGGO4AMMvRSibeyHCLINCLeV19FkHXSxD5iXTRt6WXo3vhKC/tw0XN0yNSOc1nHC+azAJxG6zOxzUzXjC9c1wxaWmGReeanlefQTHVRcMtBfm1NAOwaD0DubSTEBeRRMHfW2ZRs3nV0l73HJWh9J8+5MTpCOK7BGzC0LILMsSqpltUTLI3YmFO5Ly9RokUbcFmnn4Yu4IreTITMmn73CuLNkjZIKwucWmJkWNmWc1hrLRO12Yad/aQS+rczKcQFoNXl+bBmIAOWYJ1C6mSKG/Hw== ci_runner@gh"
     ];
   };
 
-  flowgrind_image = pkgs.dockerTools.buildImage {
-      name = "flowgrind";
-      tag = "latest";
-
-      fromImage = ubuntu;
-      fromImageName = null;
-      fromImageTag = "20.04";
-
-      runAsRoot = ''
-         #!${pkgs.runtimeShell}
-         apt-get update && apt-get install -y flowgrind
-      '';
-
-      config = {
-            Cmd = [ "sleep infinity"  ];
-      };
-  };
-
   virtualisation = {
     docker.enable = true;
-    oci-containers.containers = {
-      flowgrind = {
-        image = flowgrind_;
-        imageFile = flowgrind_image;
-      };
-    };
+    #  oci-containers.containers = {
+    #    flowgrind = {
+    #      image = "flowgrind";
+    #      imageFile = flowgrind_image;
+    #    };
+    #  };
     vswitch.enable = true;
     libvirtd = {
       enable = true;
@@ -124,5 +118,4 @@
   nixpkgs.config.allowUnfree = true;
 
   system.nixos.version = "21.11";
-
 }
