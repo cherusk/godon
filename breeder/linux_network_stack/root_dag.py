67c5dde0e73b16f584b720ff80a335950ecd4a45
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2023-03-15 10:59:36 +0100

    breeder: pass dag id correctly to subdags

--- breeder/linux_network_stack/root_dag.py
+++ breeder/linux_network_stack/root_dag.py
@@ -266,5 +266,5 @@ dag_name = config.get('name')
 
 for run_id in range(0, parallel_runs):
     dag_id = f'{dag_name}_{run_id}'
-    globals()[f'{dag_id}_optimization'] = create_optimization_dag(dag_id, config)
-    globals()[f'{dag_id}_target_interaction'] = create_target_interaction_dag(dag_id, config)
+    globals()[f'{dag_id}_optimization'] = create_optimization_dag(f'{dag_id}_optimization', config)
+    globals()[f'{dag_id}_target_interaction'] = create_target_interaction_dag(f'{dag_id}_target_interaction', config)
