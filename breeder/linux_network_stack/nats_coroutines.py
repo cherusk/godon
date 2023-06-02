d18cb3fb3986ad62a6b99404ddf0e9086d65d023
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2023-06-02 20:02:26 +0200

    consolidate nats communication coroutines
    Communication coroutines can be abstracted accross optimization and
    effectuation dag purposes.
    
    Jinja includes are the vehicle, especially necessary to keep the
    optimization objective function working.

---
+++ breeder/linux_network_stack/nats_coroutines.py
@@ -0,0 +1,34 @@
+
+{% raw %}
+async def send_msg_via_nats(subject=None, data_dict=None):
+    import time
+    import nats
+    import sys
+    # Connect to NATS Server.
+    nc = await nats.connect(NATS_SERVER_URL)
+    transmit_data = bytes(json.dumps(data_dict), encoding='utf8')
+    while True:
+        try:
+            response = await nc.request(f'{subject}', transmit_data)
+            print('Response:', response )
+            break
+        except nats.errors.NoRespondersError:
+            time.sleep(2)
+            continue
+        except:
+            logger.warning('unexpted exception')
+            logger.warning(sys.exc_info()[0])
+            raise
+    await nc.flush()
+    await nc.close()
+{% endraw %}
+
+async def receive_msg_via_nats(subject=None, timeout=300):
+    # Connect to NATS Server.
+    nc = await nats.connect(NATS_SERVER_URL)
+    sub = await nc.subscribe(f'{subject}')
+    msg = await sub.next_msg(timeout=timeout)
+    print(msg)
+    await msg.respond(b'OK')
+    await nc.close()
+    return msg.data.decode()
--- breeder/linux_network_stack/optimization.py
+++ breeder/linux_network_stack/optimization.py
@@ -4,40 +4,7 @@ def objective(trial):
 ###--- definition coroutines ---###
 ### We have to keep to coroutines in the objective function,
 ### otherwise the workers do not know about those until we preinstall those.
-{% raw %}
-    async def do_effectuation(settings):
-        import time
-        import nats
-        import sys
-        # Connect to NATS Server.
-        nc = await nats.connect(NATS_SERVER_URL)
-        settings_data = dict(settings=settings)
-        transmit_data = bytes(json.dumps(settings_data), encoding='utf8')
-        while True:
-            try:
-                response = await nc.request('effectuation', transmit_data)
-                print('Response:', response )
-                break
-            except nats.errors.NoRespondersError:
-                time.sleep(2)
-                continue
-            except:
-                logger.warning('unexpted exception')
-                logger.warning(sys.exc_info()[0])
-                raise
-        await nc.flush()
-        await nc.close()
-{% endraw %}
-
-    async def gather_recon():
-        # Connect to NATS Server.
-        nc = await nats.connect(NATS_SERVER_URL)
-        sub = await nc.subscribe('recon')
-        msg = await sub.next_msg(timeout=300)
-        print(msg)
-        await msg.respond(b'OK')
-        await nc.close()
-        return msg.data.decode()
+    {% include 'nats_coroutines.py' %}
 ###--- end coroutines ---###
 
     import logging
@@ -58,10 +25,11 @@ def objective(trial):
     settings = '\n'.join(settings)
 
     logger.warning('doing effectuation')
-    asyncio.run(do_effectuation(settings))
+    settings_data = dict(settings=settings)
+    asyncio.run(send_msg_via_nats(subject='effectuation', data_dict=settings_data))
 
     logger.warning('gathering recon')
-    metric = json.loads(asyncio.run(gather_recon()))
+    metric = json.loads(asyncio.run(receive_msg_via_nat(subject='recon')))
     metric_value = metric.get('metric')
     rtt = float(metric_value['tcp_rtt'])
     delivery_rate = float(metric_value['tcp_delivery_rate_bytes'])
