9d84a39db9594a0b951142866a2132b957ed70d1
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2023-08-12 21:30:41 +0200

    draft create table and create trigger query

--- api/archive_db.py
+++ api/archive_db.py
@@ -21,3 +21,30 @@ class archive_db():
             if db_connection:
                 db_connection.close()
                 print("Closed connection.")
+
+class queries():
+
+    def create_breeder_table(table_name=None):
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name}
+        (
+        setting_id bpchar NOT NULL,
+        setting_full jsonb NOT NULL,
+        setting_result FLOAT NOT NULL,
+        PRIMARY KEY (setting_id HASH)
+        );
+        """
+
+        return query
+
+    def create_trigger(trigger_name=None, table_name=None, function_name=None):
+        query = f"""
+        CREATE TRIGGER {trigger_name}
+        AFTER INSERT ON {table_name}
+        FOR EACH ROW
+        EXECUTE
+        FUNCTION {function_name}
+        );
+        """
+
+        return query
