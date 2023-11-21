3b55b3b9140aa6bd1ac9bb39205e677843ce2563
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2023-11-21 23:14:51 +0100

    breeder recon correct objective deref

--- breeder/linux_network_stack/effectuation.py
+++ breeder/linux_network_stack/effectuation.py
@@ -61,7 +61,7 @@ def create_target_interaction_dag(dag_id, config, target, identifier):
 
                 if recon_service_type == 'prometheus':
                     recon_query = objective.get('reconaissance').get('query')
-                    query_name = objective.get('reconaissance').get('name')
+                    query_name = objective.get('name')
 
                     query_result = prom_conn.custom_query(recon_query)
                     metric_value = query_result[0]
